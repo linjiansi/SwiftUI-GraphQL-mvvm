0816feba8829e0c2ab608dbb01c38ae45f56d0fd
linjiansi <pmon.ken.1467@gmail.com>
2021-10-03 23:44:09 +0900

    fix view name

--- SwiftUISampleApp/SwiftUISampleAppApp.swift
+++ SwiftUISampleApp/SwiftUISampleAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUISampleAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SearchRepositoryView()
         }
     }
 }
