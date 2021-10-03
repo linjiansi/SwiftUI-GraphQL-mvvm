2a3835604640384aee28cbfe0aa8938b6e9808bf
linjiansi <pmon.ken.1467@gmail.com>
2021-10-03 23:38:14 +0900

    add api client

---
+++ SwiftUISampleApp/API/APIClient.swift
@@ -0,0 +1,40 @@
+//
+//  APIClient.swift
+//  SwiftUISampleApp
+//
+//  Created by 林健司 on 2021/10/03.
+//
+
+import Foundation
+import Apollo
+
+struct APIClient {
+
+    static func request<T: Request>(
+        item: T,
+        cachePolicy: CachePolicy = .returnCacheDataElseFetch,
+        queue: DispatchQueue = .main,
+        completion: @escaping GraphQLResultHandler<T.Query.Data>
+    ) {
+
+        let store = ApolloStore(cache: InMemoryNormalizedCache())
+
+        let networkTransport = RequestChainNetworkTransport(
+            interceptorProvider: DefaultInterceptorProvider(store: store),
+            endpointURL: item.url!,
+            additionalHeaders: item.headers
+        )
+
+        let apolloClient = ApolloClient(
+            networkTransport: networkTransport,
+            store: store
+        )
+
+        apolloClient.fetch(
+            query: item.query,
+            cachePolicy: cachePolicy,
+            queue: queue,
+            resultHandler: completion
+        )
+    }
+}
